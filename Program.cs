c512e9bedfc090c9f6c2aa117008981a4b57f700
Matej Dolecek <1179@adc.itgymnazium.cz>
2023-11-23 09:56:58 +0100

    Git test update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+string g = "";
+bool alfa = true;
+while (alfa)
+{
+    Console.WriteLine("zadej slovo");
+    string a = Console.ReadLine();
+    g += a;
+    g += " ";
+    if (string.IsNullOrWhiteSpace(a))
+    {
+        alfa = false;
+        Console.WriteLine(g);
+    }
+   
+}
+/*
+string b = Console.ReadLine();
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine(b);
+}
+bool beta = true;
+while (beta)
+{
+    int d = 1;
+    string c = Console.ReadLine();
+    for (int i = 1; i == d; i++)
+    {
+        Console.WriteLine(c);
+    }
+    d++;
+}
+*/
